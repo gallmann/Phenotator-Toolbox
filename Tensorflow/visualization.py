31aaeafa46be244c71106aa25b081dd84d370081
johannesgallmann <johannesgallmann@gmail.com>
2019-05-28 15:24:11 +0200

    fixed a bug in visualization.py

--- Tensorflow/visualization.py
+++ Tensorflow/visualization.py
@@ -9,9 +9,9 @@ data has to be provided.
 """
 
 
-input_folder = "C:/Users/johan/Desktop/output/images/train"
+input_folder = "G:/Johannes/Data/May_23/MaskedAnnotationData"
 
-output_folder = "C:/Users/johan/Desktop/vis_im"
+output_folder = "G:/Johannes/vis_im"
 
 
 
@@ -45,23 +45,22 @@ def draw_bounding_boxes(input_folder, output_folder):
             annotations = annotation_data["annotatedFlowers"]
             for flower in annotations:
                 flower_name = file_utils.clean_string(flower["name"])
-                [left,right,top,bottom] = flower_info.get_bbox(flower)
+                [top,left,bottom,right] = flower_info.get_bbox(flower)
                 if not flower_name in flowers:
                     flowers.append(flower_name)
                 col = get_color_for_index(flowers.index(flower_name))
-                visualization_utils.draw_bounding_box_on_image(image,top,left,bottom,right,display_str_list=(), color=col, use_normalized_coordinates=False, thickness=1)
+                visualization_utils.draw_bounding_box_on_image(image,top,left,bottom,right,display_str_list=(flower_name), color=col, use_normalized_coordinates=False, thickness=1)
                 
         
         elif annotation_path_xml and os.path.isfile(annotation_path_xml):
             annotations = file_utils.get_annotations_from_xml(annotation_path_xml)
             for flower in annotations:
                 flower_name = file_utils.clean_string(flower["name"])
-                [left,right,top,bottom] = flower["bounding_box"]
+                [top,left,bottom,right] = flower["bounding_box"]
                 if not flower_name in flowers:
                     flowers.append(flower_name)
                 col = get_color_for_index(flowers.index(flower_name))
                 visualization_utils.draw_bounding_box_on_image(image,top,left,bottom,right,display_str_list=(), color=col, use_normalized_coordinates=False, thickness=1)
-
 
         image_name = os.path.basename(image_path)
         image.save(os.path.join(output_folder,image_name))
