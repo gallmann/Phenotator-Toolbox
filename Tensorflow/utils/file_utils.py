1639789b6d8be8ebee99a6a99cb2ced0aebfd035
Johannes <johannesgallmann@gmail.com>
2019-06-27 14:37:25 +0200

    started implementing labelme annotations export function

--- Tensorflow/utils/file_utils.py
+++ Tensorflow/utils/file_utils.py
@@ -13,6 +13,8 @@ import os
 import json
 import shutil
 import xml.etree.cElementTree as ET
+from PIL import Image
+import flower_info
 
 
 
@@ -93,7 +95,16 @@ def get_annotations_from_xml(xml_path):
     return annotations
 
     
-    
+
+def annotations_to_labelme_file(annotations,output_path,image_path):
+    image = Image.open(image_path)    
+    width, height = image.size
+    label_me_dict_template = {"version":"3.15.2","flags":{},"shapes":[],"lineColor":[0,255,0,64],"fillColor":[255,0,0,64],"imagePath":os.path.basename(output_path)[:-5] + ".png", "imageData":None,"imageHeight":height,"imageWidth":width}
+    for flower in annotations:
+        flower_dict = {"label":flower["name"], "line_color":None,"fill_color":None,"points":[],"shape_type":"polygon","flags":{}}
+        [top,left,bottom,right] = flower_info.get_bbox(flower)
+        
+    save_json_file(label_me_dict_template,output_path)
     
     
 def check_all_json_files_in_folder(folder_path):
@@ -103,6 +114,5 @@ def check_all_json_files_in_folder(folder_path):
     print("if no errors were printed, everything is fine")
     
 
-    
     
     
